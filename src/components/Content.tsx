97a7ebd87b3b5306fa58f119deaf757973026073
Flimatoledo <felip_elima@hotmail.com>
2021-03-21 22:52:27 -0300

    ending the challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+import {MovieProps} from '../App'
+
+interface ContentProps {
+selectedGenre:{
+  title: string
+},
+movies: MovieProps[];
+}
+export function Content({ movies, selectedGenre }: ContentProps) {
   // Complete aqui
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.Title}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
